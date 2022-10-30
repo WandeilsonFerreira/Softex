6c087c61a65433dacc71c6f17f4a1883d036fe6b
WandeilsonFerreira <wandeilson.ferreira@gmail.com>
2022-10-30 17:10:25 -0300

    Resolução da segunda atividade da unidade dois de React

---
+++ React/Módulo 02/atividade02/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/HelloWord'
+import HelloWord from './components/HelloWord';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+        <HelloWord />
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Módulo 02/atividade02/src/components/HelloWord.js
@@ -0,0 +1,9 @@
+function HelloWord(){
+    return(
+        <div>
+            <h1>Meu primeiro componente em react</h1>
+        </div>
+    );
+}
+
+export default HelloWord
